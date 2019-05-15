3f873533604e4c3476f4aaf186c32c6579d61332
Tun Aung Paing <lunaticrock.89@gmail.com>
2019-05-15 16:00:10 +0630

    added skills section

--- js/script.js
+++ js/script.js
@@ -12,4 +12,40 @@ $(document).ready(function() {
     startDelay: 1000,
     showCursor: false
   });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+
+  var skill = $(".skillsSection").offset().top;
+
+  $(window).scroll(function() {
+    if (window.pageYOffset > skill - $(window).height() + 250) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 150,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find("percent")
+            .text(Math.round(percent));
+        }
+      });
+    }
+  });
 });
